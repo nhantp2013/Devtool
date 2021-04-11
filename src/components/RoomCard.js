3eabdb08d5bfa7a3386a0926c9ccbda32aa555b2
nhantp2013 <nhantp2013@gmail.com>
2021-04-11 14:07:52 +0700

    Card

---
+++ src/components/RoomCard.js
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Paper from '@material-ui/core/Paper';
+// import Grid from '@material-ui/core/Grid';
+// import Item from "./Items";
+// const useStyles = makeStyles((theme) => ({
+// //   root: {
+// //     flexGrow: 1,
+// //   },
+// //   paper: {
+// //     padding: theme.spacing(2),
+// //     textAlign: 'center',
+// //     color: theme.palette.text.secondary,
+// //   },
+// }));
+
+// export default function AutoGrid() {
+//   const classes = useStyles();
+
+//   return (
+//     <div className={classes.root}>
+//       <Grid container spacing={6}>
+//         <Grid item sm={12} md={4}>
+//           <Item/>
+//         </Grid>
+        
+       
+//       </Grid>
+//     </div>
+//   );
+// }
